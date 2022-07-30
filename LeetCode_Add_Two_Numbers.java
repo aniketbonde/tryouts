81555d87c8624631543f53ebbbf4551610e98591
Aniket.Bonde <aniket.bonde@communitybrands.com>
2022-07-30 15:20:42 +0530

    add two number reverse

--- LeetCode_Add_Two_Numbers.java
+++ LeetCode_Add_Two_Numbers.java
@@ -1,2 +1,46 @@
 public class LeetCode_Add_Two_Numbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode n3 = new ListNode(2);
+        ListNode n2 = new ListNode(4,n3);
+        ListNode n1 = new ListNode(3,n2);
+
+        ListNode n6 = new ListNode(5);
+        ListNode n5 = new ListNode(6,n6);
+        ListNode n4 = new ListNode(4,n5);
+
+        System.out.println(addTwoNumbers(n1,n4));
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
 }
